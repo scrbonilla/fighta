be4bd055a2d3b3b679ccb6042bd7de838ed53297
scrbonilla <scrbonilla84@gmail.com>
2014-10-29 15:52:07 -0400

    Update schema; rails g resource video

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029092004) do
+ActiveRecord::Schema.define(version: 20141029194743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20141029092004) do
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "videos", force: true do |t|
+    t.string   "link"
+    t.string   "title"
+    t.string   "author"
+    t.string   "duration"
+    t.integer  "likes"
+    t.integer  "dislikes"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
