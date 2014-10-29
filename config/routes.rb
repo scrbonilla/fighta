ba14b4b8a570ee845bb339dc8cbf277c28c51044
scrbonilla <scrbonilla84@gmail.com>
2014-10-29 16:26:55 -0400

    Trying to get working page; still have errors; authentication and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to:'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  # get 'sessions/new'
+
+  # get 'signup', to: 'users#new', as: 'signup'
+  # get 'login', to:'sessions#new', as: 'login'
+  # get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
   resources :sessions
+
+
+  root to: 'videos#index'
+  resources :videos, only: [:index, :new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
