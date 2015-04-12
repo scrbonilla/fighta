e7a0be5e7d5ac8af4ce39be744ae31b699a3fa15
scrbonilla <scrbonilla84@gmail.com>
2015-04-12 10:01:09 -0400

    adjust spacing on private method def authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
     before_action :require_signin
 end
 
-# private
-#    def current_user
-#        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-#    end
+private
+   def current_user
+       @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   end
 
-#    def authorize
-#        redirect_to login_url, alert: "Not authorized" if current_user.nil?
-#    end
+   def authorize
+       redirect_to login_url, alert: "Not authorized" if current_user.nil?
+   end
